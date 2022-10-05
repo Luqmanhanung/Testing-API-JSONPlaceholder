33447982e7507d36445e260f1f042ce98972cd6d
Luqman Hanung Asidiq <luqmanhanung@gmail.com>
2022-10-05 13:26:38 +0700

    first template

---
+++ src/test/java/JSONPlaceholder/JsonplaceholderAPI.java
@@ -0,0 +1,8 @@
+package JSONPlaceholder;
+
+public class JsonplaceholderAPI {
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    public static final String DIR = System.getProperty("user.dir");
+    public static final String JSON_FILE = DIR+"/src/test/resources/Jsonfile";
+
+}
